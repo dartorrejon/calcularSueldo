803158ac4d0ef9060a25ddcc8088ed2a714693ec
Torrejon Dario Rodrigo <dartorrejon432@outlook.com>
2023-05-29 23:05:40 -0300

    Agregamos los feriados consumiendo una Api REST

---
+++ Scripts/feriados.js
@@ -0,0 +1,33 @@
+const año = new Date().getFullYear();
+const nombresMeses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+];
+let tituloFeriados = document.querySelector('title');
+tituloFeriados.innerText = `Feriados ${año}` //agregamos el año actual al titulo
+
+const feriados = fetch(`http://nolaborables.com.ar/api/v2/feriados/${año}`);
+
+    feriados
+    .then(res => res.json())
+    .then(data => {
+        const ul = document.createElement('ul');
+        //Obtenemos toda la informacion de los feriados recorriendolo con un forEach
+        Object.keys(data).forEach(function (diaFeriado) {
+            const feriado = data[diaFeriado];
+            const li = document.createElement('li');
+            li.innerText += `${feriado.dia} de ${nombresMeses[(feriado.mes - 1)]} : ${feriado.motivo} (${feriado.tipo})`;
+            ul.appendChild(li);
+        })
+        document.querySelector('#contenedor-feriados').appendChild(ul);
+    })
